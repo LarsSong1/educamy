83443358e5b5b0050a3c6b885ca08f0b322ed3a2
LarsSong1 <damianalejandrofirst@gmail.com>
2025-04-26 16:53:14 -0500

    generate function no ye

--- educamy/admin.py
+++ educamy/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Materia
+from .models import Materia, ContenidoGenerado
 
 # Register your models here.
 admin.site.register(Materia)
+admin.site.register(ContenidoGenerado)
